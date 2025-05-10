919e58cba6448802588cecbe378d9d1455a90fe4
Natalie Childs <nata.faye@gmail.com>
2025-05-10 10:52:49 -0700

    tic-tac-toe but actually just a dismissable alert

--- scratch.js
+++ scratch.js
@@ -1,92 +1,79 @@
-const WORD_OPTIONS = ["FADE", "CAB", "BAD", "BADE"]
-const targetIndex = Math.floor(Math.random() * WORD_OPTIONS.length)
-const targetWord = WORD_OPTIONS[targetIndex]
+// Tic Tac Toe
 
-const guessedLetters = []
+// get the messages container
+const messagesContainer = document.querySelector("#messages-container")
 
+function createMessage() {
+    // make the paragraph
+    const messageP = document.createElement("p")
 
-/** Make the target word area **/
+    messageP.className = "bg-primary-subtle p-3 rounded"
+    messageP.style.color = "white"
+    messageP.textContent = "Goodbye!"
 
-const targetWordParagraph = document.querySelector("#target-word")
-// const targetWordParagraph = document.getElementById("target-word")
+    // put a button inside the paragraph
+    const babyButton = document.createElement("button") // made the soul
 
-let targetWordUnderlines = ""
-for (let i = 0; i < targetWord.length; i++) {
-    targetWordUnderlines += "_ "
+    babyButton.textContent = "X"
+    babyButton.className = "btn btn-primary btn-sm float-end"
+
+    babyButton.addEventListener("click", () => {
+        // remove the paragraph
+        messageP.remove()
+    })
+
+    messageP.appendChild(babyButton) // moved it to earth (the page)
+
+    // add the messageP to messagesContainer
+    messagesContainer.appendChild(messageP)
+
+    const clonedMessageTrue = messageP.cloneNode(true)
+    const clonedMessageFalse = messageP.cloneNode()
+
+    //console.log("true", clonedMessageTrue)
+    //console.log("false", clonedMessageFalse)
+    // Two options
+    // cloneNode(true)
+    // cloneNode()
 }
 
-targetWordParagraph.innerHTML = targetWordUnderlines
+function clearMessages() {
+    // get rid of any messages inside of the messages-container
 
+    // 3rd Place
+    // while(messagesContainer.lastChild) {
+    //     messagesContainer.removeChild(messagesContainer.lastChild)
+    // }
 
-/** Make the letter buttons **/
+    // 2nd Place
+    messagesContainer.innerHTML = ""
 
-const ALPHABET = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-
-// <button onclick="handleGuess('A')">A</button>
-
-"letter-buttons-box"
-// Find the div
-const buttonsBox = document.getElementById("letter-buttons-box")
-
-//for(let letter of ALPHABET) {
-//while(i < ALPHABET.length) {
-//for(let i = 0; i < ALPHABET.length; i++) {
-//forEach(letter => {})
-
-for(let i = 0; i < ALPHABET.length; i++) {
-    // make a button with the letter in it
-    const letterButton = document.createElement("button") // make a new little button soul
-
-    // put the letter as the text of the button
-    letterButton.innerHTML = ALPHABET[i]
-
-    // hook up the listener
-    letterButton.addEventListener("click", (event) => handleGuess( event, ALPHABET[i] ) )
-
-    // if you need to pass a parameter to a function that you're setting with addEventListener
-    // you wrap it in another function that calls the first function with the parameter
-
-    // put that button in the buttonsBox
-    // the way to give something an earth pass (aka move it from the Great Before to the page (earth))
-    buttonsBox.appendChild(letterButton)
-
-    // vaguely the innerHTML version of this would look like this:
-    // buttonsBox.innerHTML += "<button class='my-button-class'>" + ALPHABET[i] + "</button>"
-    // also hook up the event listener to that button which is a lil tricky
+    // 1st Place
+    // something.replaceChildren()
 }
 
-// You kind of have two options: using innerHTML or using document.createElement to get new elements in the page <button></button>
+// Add click event listener to clear button that calls clearMessage function
+// .addEventListener(event, functionToCall)
+const clearButton = document.querySelector("#clear-button")
+clearButton.addEventListener("click", clearMessages)
+
+// the difference between
+
+// sending someone the phone number for them to call = clearMessages
+// give them the function itself
+
+// calling that contact and sending them what the contact said = clearMessages()
+// give them what the function returns
 
 
+function showSparkles(event) {
+    const div = document.createElement("div")
+    div.textContent = "✨"
+    div.className = "position-absolute"
 
-// In the past we did this:
-// prompting them for a guess
-// handling their guess
+    // I would like left and right to be where you are clicking
+    div.style.top = event.pageY + "px"
+    div.style.left = event.pageX + "px"
 
-// But in professional code we do this:
-// listen for a guess
-// handle their guess
-
-// a lil funky with the event, probably more complicated then you'll run into
-function handleGuess(event, guessLetter) {
-    guessedLetters.push(guessLetter)
-
-    let targetWordUnderlines = ""
-    for (let i = 0; i < targetWord.length; i++) {
-        if (guessedLetters.includes(targetWord[i])) {
-            // if the letter were checking is in the guessedLetters array
-            targetWordUnderlines += targetWord[i] + " "
-        } else {
-            // if they haven't guessed it yet, put an underline
-            targetWordUnderlines += "_ "
-        }
-    }
-
-    targetWordParagraph.innerHTML = targetWordUnderlines
-
-    // remove the button that they just clicked
-    event.target.remove()
+    document.body.appendChild(div)
 }
